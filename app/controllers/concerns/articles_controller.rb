b415cd5f8a15f2d74d369bdb58a48d5ee0303228
princedagar25 <dagarprince900@gmail.com>
2025-02-16 09:27:49 +0530

    adding new article form local

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,5 +1,20 @@
 class ArticlesController < ApplicationController
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
-end
+  
+    def index
+      @articles = Article.all
+    end
+  
+    def new
+      
+    end
+  
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
+    end
+    
+  end
